ae7659f09a30cc06a42cac6df8b7e9fd887caa99
FerClososki <fernandaclososki@hotmail.com>
2026-03-16 16:15:43 -0300

    Arrumando caminho de pastas e colocando link para numero de WhatsApp

---
+++ projeto/javascript.js
@@ -0,0 +1,23 @@
+document.getElementById("form-agendamento").addEventListener("submit", function(e){
+
+e.preventDefault();
+
+let nome = document.getElementById("nome").value
+let whats = document.getElementById("whats").value
+let servico = document.getElementById("servico").value
+let data = document.getElementById("data").value
+
+let mensagem = `Olá! Gostaria de agendar um horário.
+
+Nome: ${nome}
+WhatsApp: ${whats}
+Serviço: ${servico}
+Data desejada: ${data}`
+
+let numero = "5541987426394"
+
+let url = `https://wa.me/${+5541987426394}?text=${encodeURIComponent(mensagem)}`
+
+window.open(url, "_blank")
+
+})
